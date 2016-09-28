c5c3047fa1a8d962c440e9a7f2b5367b408ed201
gitjhall <gitjhall@users.noreply.github.com>
2016-09-28 17:26:56 -0400

    Removed imports and bindings for layers other than ZWave

--- setup/scapy/layers/gnuradio.py
+++ setup/scapy/layers/gnuradio.py
@@ -9,10 +9,6 @@ Gnuradio layers, sockets and send/receive functions.
 """
 
 from scapy.layers.ZWave import *
-from scapy.layers.dot15d4 import *
-from scapy.layers.bluetooth4LE import *
-from scapy.layers.wmbus import *
-
 
 class GnuradioPacket(Packet):
     name = "Gnuradio header"
@@ -27,17 +23,5 @@ class GnuradioPacket(Packet):
 #bind_layers(GnuradioPacket, ZWave, proto=1)
 bind_bottom_up(GnuradioPacket, ZWave, proto=1)
 bind_top_down(GnuradioPacket, ZWaveReq, proto=1)
-
-## ZigBee
-bind_layers(GnuradioPacket, Dot15d4FCS, proto=2)
-
-## Bluetooth 4 LE
-bind_layers(GnuradioPacket, BTLE, proto=3)
-
-## WMBus
-bind_layers(GnuradioPacket, WMBus, proto=4)
-
-## Dash7
-#bind_layers(GnuradioPacket, Dash7, proto=5)
 
 conf.l2types.register(148, GnuradioPacket)
